30bc3ae29ad2b31909608832ee66b80f74b68bd2
WEN <WEN>
2017-11-30 17:41:25 -0800

    expanded script_ordering

--- script_ordering.py
+++ script_ordering.py
@@ -1,17 +1,39 @@
 import os
 
-testDirIn = "./inputs/inputs_staff/"
-testDirOut = "./outputs/outputs_staff/"
+staffDirIn = "./inputs/inputs_staff/"
+staffDirOut = "./outputs/outputs_staff/"
+In20 = "./inputs/inputs20/"
+Out20 = "./outputs/outputs20/"
+In35 = "./inputs/inputs35/"
+Out35 = "./outputs/outputs35/"
+In50 = "./inputs/inputs50/"
+Out50 = "./outputs/outputs50/"
 
-# wizard_ordering.main(testIn, testOut)
-# os.system("python wizard_ordering.py {0} {1}".format(testIn, testOut))
+# Executes wizard_ordering.py for a series of input files
+# Input file locations must be relative to wizard_ordering.py as described above
+# Comment out and edit ranges as necessary
 
+# For Input 20s
+for i in range(10):
+	inputFile = In20 + "input20_" + str(i) + ".in"
+	outputFile = Out20 + "input20_" + str(i) + ".out"
+	os.system("python wizard_ordering.py {0} {1}".format(inputFile, outputFile))
+
+# For Input 35s
+for i in range(10):
+	inputFile = In35 + "input35_" + str(i) + ".in"
+	outputFile = Out35 + "input35_" + str(i) + ".out"
+	os.system("python wizard_ordering.py {0} {1}".format(inputFile, outputFile))
+
+# For Input 50s
+for i in range(10):
+	inputFile = In50 + "input50_" + str(i) + ".in"
+	outputFile = Out50 + "input50_" + str(i) + ".out"
+	os.system("python wizard_ordering.py {0} {1}".format(inputFile, outputFile))
+
+# For Staff Inputs
 for i in range(3, 21):
 	j = i * 20
-	inputFile = testDirIn + "staff_" + str(j) + ".in"
-	print("Input File: {0}".format(inputFile))
-
-	outputFile = testDirOut + "staff_" + str(j) + ".out"
-	print("Output File: {0}".format(outputFile))
-
+	inputFile = staffDirIn + "staff_" + str(j) + ".in"
+	outputFile = staffDirOut + "staff_" + str(j) + ".out"
 	os.system("python wizard_ordering.py {0} {1}".format(inputFile, outputFile))
