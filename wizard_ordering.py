d2f14ba55d10e6e29bc034c93af4c83120448171
WEN <WEN>
2017-11-30 16:40:48 -0800

    added output35 files, cleaned ordering file

--- wizard_ordering.py
+++ wizard_ordering.py
@@ -30,7 +30,7 @@ def solve(num_wizards, num_constraints, wizards, constraints, filename, outfile)
         f.write("prob = LpProblem(\"wiz\", LpMaximize)\n\n")
 
         f.write("start = time.time()\n")
-        f.write("smallC = 0.05\n")
+        f.write("smallC = 1\n")
         f.write("bigM = {0}\n".format(5 + num_wizards))
         f.write("wizzies = set()\n")
         # wizard variables
@@ -63,7 +63,8 @@ def solve(num_wizards, num_constraints, wizards, constraints, filename, outfile)
             f.write("prob += {0} >= smallC + {1} - (1 - {2}) * bigM\n\n".format(x_1, x_3, z_1))
 
         f.write("\n")
-        f.write("GLPK().solve(prob)\n")
+        # f.write("GLPK().solve(prob)\n")
+        f.write("prob.solve(pulp.PULP_CBC_CMD())\n")
         f.write("ages = {}\n")
         f.write("for v in prob.variables():\n")
         f.write("\tif v.name in wizzies:\n")
@@ -74,33 +75,10 @@ def solve(num_wizards, num_constraints, wizards, constraints, filename, outfile)
         f.write("with open(\"{0}\", \"w\") as f:\n".format(outfile))
         f.write("\tfor i in range({0}):\n".format(num_wizards))
         f.write("\t\tf.write(\"{0} \".format(sorted_ages[i][0]))\n")
-        # f.write("relative_mapping = {}\n")
-        # f.write("for i in range(len(sorted_ages)):\n")
-        # f.write("\trelative_mapping[sorted_ages[i][0]] = i\n")
+
     os.system("python {0}".format(filenameLP))
+    os.remove(filenameLP)
 
-
-
-# def num_satisfied_constraints(constraints, num_constraints):
-#     wizard_indices = {'Neal': 0, 'Conner': 1, 'Janette': 2, 'Amari': 3, 'Carla': 4, 'Curt': 5, 'Chase': 6, 'Ayesha': 7, 'Gene': 8, 'Dominick': 9, 'Joni': 10, 'Pam': 11, 'Wesley': 12, 'Mckenna': 13, 'Lianne': 14, 'Skylar': 15, 'Ruben': 16, 'Paris': 17, 'Daragh': 18, 'Oisin': 19}
-#
-#     num_satisfied = 0
-#
-#     for i in range(num_constraints):
-#         constraint = constraints[i]
-#         left_index = min(wizard_indices[constraint[0]], wizard_indices[constraint[1]])
-#         right_index = max(wizard_indices[constraint[0]], wizard_indices[constraint[1]])
-#         wiz_index = wizard_indices[constraint[2]]
-#
-#         if (wiz_index < left_index) or (wiz_index > right_index):
-#             num_satisfied += 1
-#
-#     return num_satisfied
-
-# def write_output(filename, solution):
-#     with open(filename, "w") as f:
-#         for wizard in solution:
-#             f.write("{0} ".format(wizard))
 
 # python wizard_ordering.py ./inputs/inputs20/input20_1.in ./outputs/outputs20/output20_1.out
 if __name__ == "__main__":
@@ -113,6 +91,4 @@ if __name__ == "__main__":
     solve(num_wizards, num_constraints, wizards, constraints, args.input_file, args.output_file)
     constraints_satisfied, num_constraints, constraints_failed = output_validator.processInput(args.input_file, args.output_file)
     print("You satisfied {}/{} constraints. List of failed constraints: {}".format(constraints_satisfied, num_constraints, constraints_failed))
-    # print(num_satisfied_constraints(constraints, num_constraints))
-    # write_output(args.output_file, solution)
 
