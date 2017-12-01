1a42c4ae7ec677a690f393cdf3adab6969267cf9
Xintong Wen <wenb@berkeley.edu>
2017-11-30 17:16:13 -0800

    staff 60 to 140 added, changed script to look at staff input

--- script_ordering.py
+++ script_ordering.py
@@ -1,16 +1,17 @@
 import os
 
-testDirIn = "./inputs/inputs50/"
-testDirOut = "./outputs/outputs50/"
+testDirIn = "./inputs/inputs_staff/"
+testDirOut = "./outputs/outputs_staff/"
 
 # wizard_ordering.main(testIn, testOut)
 # os.system("python wizard_ordering.py {0} {1}".format(testIn, testOut))
 
-for i in range(3, 10):
-	inputFile = testDirIn + "input50_" + str(i) + ".in"
+for i in range(3, 21):
+	j = i * 20
+	inputFile = testDirIn + "staff_" + str(j) + ".in"
 	print("Input File: {0}".format(inputFile))
 
-	outputFile = testDirOut + "output50_" + str(i) + ".out"
+	outputFile = testDirOut + "staff_" + str(j) + ".out"
 	print("Output File: {0}".format(outputFile))
 
 	os.system("python wizard_ordering.py {0} {1}".format(inputFile, outputFile))
